636fdf1fc8050f619e5299892c1ac0b95fd6677f
Sam Deane <sam@elegantchaos.com>
2022-07-19 12:42:26 +0100

    Upped watchOS minimum to v6.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "JSONSession",
     
     platforms: [
-        .macOS(.v10_15), .macCatalyst(.v13), .iOS(.v13), .tvOS(.v13), .watchOS(.v5)
+        .macOS(.v10_15), .macCatalyst(.v13), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)
     ],
     products: [
         .library(
